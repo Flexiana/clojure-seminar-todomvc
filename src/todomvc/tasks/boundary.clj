2600200de21d874481f95de7b9a93c68dfdf0f62
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2018-04-17 18:21:18 +0200

    Add create-task and finish-task functions

--- src/todomvc/tasks/boundary.clj
+++ src/todomvc/tasks/boundary.clj
@@ -11,4 +11,10 @@
 (extend-protocol TaskService
   duct.database.sql.Boundary
   (get-all [db-spec]
-    (db/get-all (:spec db-spec))))
+    (db/get-all (:spec db-spec)))
+
+  (create-task [db-spec task]
+    (db/create-task (:spec db-spec) task))
+
+  (finish-task [db-spec id]
+    (db/finish-task (:spec db-spec) {:id id})))
--- src/todomvc/tasks/db.sql
+++ src/todomvc/tasks/db.sql
@@ -1,2 +1,10 @@
 -- :name get-all :? :*
 select * from tasks
+
+
+-- :name create-task :<! :1
+insert into tasks (title, description) values (:title, :description) returning *
+
+
+-- :name finish-task :<! :1
+update tasks set done = true where id = :id returning *
