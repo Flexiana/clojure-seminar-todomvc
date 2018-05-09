19538d8818712a29743b979a32861b6b3d97109c
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2018-05-09 17:37:28 +0200

    Add tests for create-task

--- test/todomvc/tasks/api_test.clj
+++ test/todomvc/tasks/api_test.clj
@@ -1,6 +1,6 @@
 (ns todomvc.tasks.api-test
   (:require
-    [clojure.test :refer [deftest is]]
+    [clojure.test :refer [deftest is testing]]
     [cheshire.core :as cheshire]
     [integrant.core :as ig]
     [ring.mock.request :as mock]
@@ -9,18 +9,37 @@
 
 
 (defn json-response
-  [conf request]
-  (let [handler (ig/init-key :todomvc.handler/api conf)]
-    (-> request
-        handler
-        (update :body #(some-> % (cheshire/decode true))))))
+  ([conf method path]
+   (json-response conf method path nil))
+  ([conf method path body]
+   (let [handler (ig/init-key :todomvc.handler/api conf)
+         request (mock/request method path)]
+     (-> (if (nil? body)
+           request
+           (assoc request :body-params body))
+         handler
+         (update :body #(some-> % (cheshire/decode true)))))))
 
 
 (deftest get-tasks-test
   (let [conf {:db (reify TaskService
                     (get-all [db-spec]
                       []))}
-        request (mock/request :get "/api/tasks")
-        response (json-response conf request)]
+        response (json-response conf :get "/api/tasks")]
     (is (= 200 (:status response)))
     (is (= [] (:body response)))))
+
+
+(deftest create-task-test
+  (testing "should create a task"
+    (let [conf {:db (reify TaskService
+                      (create-task [db-spec params]
+                        (assoc params :id 1)))}
+          response (json-response conf :post "/api/tasks" {:title "Task 1"})]
+      (is (-> response :body :id some?))
+      (is (= 201 (:status response)))))
+
+  (testing "should return bad request"
+    (let [response (json-response {:db nil} :post "/api/tasks" {})]
+      (is (= {:errors {:title "this field is mandatory"}} (:body response)))
+      (is (= 400 (:status response))))))
